438e3257586be3b19252647a2513901520399a58
Glyn L <glyn_92@hotmail.com>
2018-05-12 12:58:33 +0700

    added responsiveness

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,21 @@ navbarItems.forEach((item) => {
     setTimeout(() => section.focus(), 500);
   });
 });
+
+const hamburger = document.querySelector('.nav__item--hamburger');
+
+hamburger.addEventListener('click', () => {
+  navbarItems.forEach((item) => {
+    if (!item.classList.contains('nav__item--hamburger') && !item.classList.contains('nav__item--left')) {
+      if (item.classList.contains('visible')) {
+        item.style.opacity = '0';
+        setTimeout(() => item.classList.remove('visible'), 500);
+      } else {
+        setTimeout(() => item.style.opacity = '0.9', 50);
+        item.classList.add('visible');
+      }
+    } else if (item.classList.contains('nav__item--hamburger')) {
+      item.classList.toggle('rotated');
+    }
+  });
+});
